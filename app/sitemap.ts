ecaad2897585a07df291e28d457b5f69183c369d
parhom8989-dotcom <parhom8989@gmail.com>
2026-01-24 08:30:42 +0300

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -9,25 +9,25 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
-      url: 'https://www.calcoria.ru/teplotekhnika', // ваша страница калькулятора
+      url: 'https://calcoria.ru/teplotekhnika', // ваша страница калькулятора
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
    {
-  url: 'https://www.calcoria.ru/mechanics', // адрес новой страницы
+  url: 'https://calcoria.ru/mechanics', // адрес новой страницы
   lastModified: new Date(), // сегодняшняя дата
   changeFrequency: 'monthly', // как часто меняется страница
   priority: 0.7, // важность от 0.1 до 1.0
 }, 
 {
-  url: 'https://www.calcoria.ru/electrotech', // адрес новой страницы
+  url: 'https://calcoria.ru/electrotech', // адрес новой страницы
   lastModified: new Date(), // сегодняшняя дата
   changeFrequency: 'monthly', // как часто меняется страница
   priority: 0.7, // важность от 0.1 до 1.0
 }, 
 {
-  url: 'https://www.calcoria.ru/prochee', // адрес новой страницы
+  url: 'https://calcoria.ru/prochee', // адрес новой страницы
   lastModified: new Date(), // сегодняшняя дата
   changeFrequency: 'monthly', // как часто меняется страница
   priority: 0.7, // важность от 0.1 до 1.0
