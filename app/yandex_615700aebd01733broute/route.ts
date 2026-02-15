20bad8f65289ecc3f6ab69899cb53884d7e28abc
Evgeniy <parhom8989@gmail.com>
2026-02-15 12:01:11 +0300

    Обновление

--- app/yandex_615700aebd01733broute/route.ts
+++ app/yandex_615700aebd01733broute/route.ts
@@ -2,7 +2,7 @@ export async function GET() {
   const htmlContent = `<!DOCTYPE html>
 <html>
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="yandex-verification" content="106587f3338e6756" />
     </head>
     <body>Verification: 106587f3338e6756</body>
 </html>`;
